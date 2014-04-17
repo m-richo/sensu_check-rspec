39f1fd39a31b7425afab94efc0b84e8c3b231a9f
Michael Richardson <michael@michaelandhelen.com>
2014-04-17 11:56:46 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 sensu_check-rspec
 =================
+
+basic script to run rspec tests and report summary results.  
+Individual test results are also sent to a local socket for sensu monitoring purposes.
+
---
+++ rspec_testing.rb
@@ -0,0 +1,26 @@
+require "rubygems"
+require "json"
+require "socket"
+
+serverspec_results = `cd /rspec_tests ; sudo ruby -S rspec spec/localhost/ --format json`
+parsed = JSON.parse(serverspec_results)
+
+parsed["examples"].each do |serverspec_test|
+  test_name = serverspec_test["file_path"].split('/')[-1] + "_" + serverspec_test["line_number"].to_s
+  output = serverspec_test["full_description"].gsub!(/\"/, '')
+  status = 0
+  if serverspec_test["status"] != "passed"
+    status = 1
+  end
+  conn = TCPSocket.new '127.0.0.1', 3030
+  conn.puts "{\"handlers\": [\"default\"], \"name\": \"#{test_name}\", \"output\": \"#{output}\", \"status\": #{status} }"
+  conn.close
+end
+
+puts parsed["summary_line"]
+failures = parsed["summary_line"].split[2]
+if failures == '0'
+  exit 0
+else
+  exit 2
+end
